e79a3fd3d17f4eaffadd98a2cb7cd0097d76f638
hyesunShin <fishcakecake@naver.com>
2021-10-05 18:11:42 +0900

    [퀴즈컴포넌트] 스타일, 구조 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import './App.css';
+import React from "react";
 
 function App() {
+    return (
+        <div className="container">
+            <div className="app">
+                <div className="question-section">
+                    <h1 className="question-header">
+                        <span>1</span>/4
+                    </h1>
+                    <div className="question-text">
+                        세계 최초로 안락사를 합법화한 나라는?
+                    </div>
+                </div>
+                <div className="answer-section">
+                    <button>미국</button>
+                    <button>영국</button>
+                    <button>네덜란드</button>
+                    <button>스위스</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
